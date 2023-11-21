fed5da878bcd95df612dd2fb495b0c4da1db66cd
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2023-11-20 17:15:44 -0700

    working on media queries for header

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import menu from '../Assets/hamburger-menu.svg';
 import './Header.css';
 
 function Header(props){
+    const [isOpen, setIsOpen] = useState(false);
+    
+
+    function menuClick(){
+        setIsOpen(!isOpen);
+    }
+
     return(
         <div className='main-header-container'>
             <div className='header-components-container'>
                 <h1>Start Bootstrap</h1>
+                <button className='smaller-screen-size-menu-toggle-bttn' onClick={() => menuClick() }>
+                    <div>Menu</div>
+                    <img className='menu-icon' src={menu}/>
+                </button>
+            </div>
+            {isOpen && (
+                <div className='menu-components-container'>
                 <ul className='menu-contents'>
                     <li>About</li>
                     <li>Projects</li>
                     <li>Contact</li>
                 </ul>
-                <button className='smaller-screen-size-menu-toggle-bttn'>
-                    <div>Menu</div>
-                    <img className='menu-icon' src={menu}/>
-                    {/* <div className='hamburger-menu'>
-                        <div className='line'></div>
-                        <div className='line'></div>
-                        <div className='line'></div>
-                    </div> */}
-                </button>
-            </div>
+                </div>
+            )}
         </div>
     )
 }
